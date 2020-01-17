9e24b92af9cd70976a751ac3ddb3b59f028567f6
7FFF00 <t42please@gmail.com>
2020-01-17 13:51:18 -0800

    DEFINE repoSchema

--- database/index.js
+++ database/index.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  id: Number,
+  name: String,
+  owner: { userId: Number, userName: String },
+  url: String,
+  description: String,
+  createdAt: String, // possibly a Date
+  updatedAt: String, // "
+  language: String
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
